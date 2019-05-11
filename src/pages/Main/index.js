a9281d48ac9ea601625ffc51e40a50d60b9b1721
Eduardo Tavares <edutav@gmail.com>
2019-05-11 18:28:03 -0400

    implementação ro resultado da busca na API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,50 +9,50 @@ import { Container, Form } from './styles';
 import CompareList from '../../components/CompareList';
 
 export default class Main extends Component {
-	state = {
-	  loading: false,
-	  repositoryError: false,
-	  repositoryInput: '',
-	  repositories: [],
-	};
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
 
-	handleAddRepository = async (e) => {
-	  e.preventDefault();
-	  this.setState({ loading: true });
-	  try {
-	    const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
 
-	    repository.lastCommit = moment(repository.pushed_at).fromNow();
-	    this.setState({
-	      repositoryInput: '',
-	      repositories: [...this.state.repositories, repository],
-	      repositoryError: false,
-	    });
-	  } catch (error) {
-	    this.setState({ repositoryError: true });
-	  } finally {
-	    this.setState({ loading: false });
-	  }
-	};
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false,
+      });
+    } catch (error) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
 
-	render() {
-	  return (
-  <Container>
-    <img src={logo} alt="App Compoare" />
-    <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
-      <input
-        type="text"
-        placeholder="usuário/repositório"
-        value={this.state.repositoryInput}
-        onChange={e => this.setState({ repositoryInput: e.target.value })}
-      />
-      <button type="submit">
-        {this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
-      </button>
-    </Form>
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="App Compoare" />
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">
+            {this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
+        </Form>
 
-    <CompareList repositories={this.state.repositories} />
-  </Container>
-	  );
-	}
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
 }
